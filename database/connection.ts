4b2d4485469a3a6626259ca52333ddef855b6150
Furtado3 <lucas_shiguioka@hotmail.com>
2021-02-04 12:57:11 -0300

    initialized the knex

---
+++ database/connection.ts
@@ -0,0 +1,13 @@
+import Knex from 'knex';
+import path from 'path';
+
+
+const db = Knex({
+    client: 'sqlite',
+    connection: {
+        filename : path.resolve(__dirname,'database.sqlite')
+    },
+    useNullAsDefault : true
+})
+
+export default db;
